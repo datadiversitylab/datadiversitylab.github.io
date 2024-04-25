59db0c266f8f65ce331f7627bbd60e0b5a3fc8b5
ColumbiaValleyWebDesigns <odb.developer@gmail.com>
2024-04-24 17:56:07 -0700

    fixes for new socials and grants. fix join us overlap and tab description for LabOps

--- src/assets/js/dropdownCards.js
+++ src/assets/js/dropdownCards.js
@@ -1,7 +1,8 @@
 var dropdowns = document.querySelectorAll("#dropdown-cards .cs-flex-group");
 
 dropdowns.forEach(function(dropdown) {
-    dropdown.addEventListener("click", function(e) {
+    clickable = dropdown.querySelector(".cs-dropdown-show")
+    clickable.addEventListener("click", function(e) {
         if (dropdown.classList.contains("is-active")) {
             dropdown.querySelector(".cs-dropdown-icon img").src = "../assets/icons/chevron-down.svg";
             dropdown.classList.remove("is-active");
--- src/socials/la-indita.md
+++ src/socials/la-indita.md
@@ -4,7 +4,7 @@ socialName: La Indita, Tucson
 socialDate: December 1 (2022)
 socialDesc: Attendees at the lab dinner organized in Fall 2022 at La Indita, Tucson, AZ. You will be able to recognize a handful of lab members (Danielle Van Boxel [from the left, second], Salena Torres [third], Cristian Román [fifth], Kyle Arechiga [seventh], Kyle Clemments [eight]), some of us with our significant others!
 tags:
-  - 2022
+  - "2022"
 image: /assets/images/social/la-indita.jpg
 imageAlt: la indita image
 ---
